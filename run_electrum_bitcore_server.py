2ce939762bf3ddb1bf4cc89212e2d7450b55f04c
David <david.bergen@gmx.net>
2019-12-28 00:47:00 +0000

    Replace deprecated imp module with importlib.util

--- run_electrum_bitcore_server.py
+++ run_electrum_bitcore_server.py
@@ -24,11 +24,19 @@ import time
 import threading
 import json
 import os
-import imp
-
 
 if os.path.dirname(os.path.realpath(__file__)) == os.getcwd():
-    imp.load_module('electrumbitcoreserver', *imp.find_module('src'))
+    name = 'electrumserver'
+    spec = importlib.util.find_spec(name)
+    if name in sys.modules:
+        print(f"{name!r} already in sys.modules")
+    elif (spec) is not None:
+        module = importlib.util.module_from_spec(spec)
+        sys.modules[name] = module
+        #spec.loader.exec_module(module)
+        print(f"{name!r} has been imported")
+    else:
+        print(f"can't find the {name!r} module")
 
 from electrumbitcoreserver import storage, networks, utils
 from electrumbitcoreserver.processor import Dispatcher, print_log
